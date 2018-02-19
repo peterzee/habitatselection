9c3dabfd7a76497f636ff28ebcceb772f9664d58
Peter Zee <peterzee@hst000171421um.bio.olemiss.edu>
2018-02-19 10:53:17 -0600

    modular landscape functions (and draft file)

---
+++ module_landscape_drafts.R
@@ -0,0 +1,42 @@
+source('plotLandscape_function.R')
+
+
+  module.dim <- 3
+  hab.matrix <- 3
+  patch.dim <- 4
+  
+  mod.index <- array(1:module.dim^2, dim = c(module.dim, module.dim))
+  tmp.ind <- which(mod.index > 0, arr.ind = TRUE)
+  
+  translateIndex <- function(x){
+    ((x-1) * (hab.matrix + patch.dim)) + 1
+  }
+  
+  quilted.index <- translateIndex(tmp.ind)
+  quilted <- array(dim = c(module.dim * (hab.matrix + patch.dim), module.dim * (hab.matrix + patch.dim)))
+  
+  count <- 0
+  
+  for (i in 1:length(mod.index)){
+    
+    count <- count + 1
+    
+    hab.module <- array(0, dim = c(hab.matrix + patch.dim, hab.matrix + patch.dim))
+    
+    # x <- array(sample(0:2, patch.dim^2, prob = c(1,1,1), replace = TRUE), dim = c(patch.dim, patch.dim))
+    x <- array(sample(0:2, patch.dim^2, prob = c(1, 1, runif(1, 0, 5)), replace = TRUE), dim = c(patch.dim, patch.dim))
+    
+    hab.module[((hab.matrix / 2) + 1) : (((hab.matrix / 2)) + patch.dim), ((hab.matrix / 2) + 1) : (((hab.matrix / 2)) + patch.dim) ] <- x
+    
+    A <- hab.module
+    
+    quilted[quilted.index[i,1] : (quilted.index[i,1] + (hab.matrix + patch.dim - 1)),
+            quilted.index[i,2] : (quilted.index[i,2] + (hab.matrix + patch.dim - 1))] <- A
+    
+  }
+  
+
+plotLandscape(quilted)
+
+
+
