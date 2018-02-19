b918ad7aa1b9c3415a012b4e901a4bb5dd1a5eca
Peter Zee <peterzee@hst000171421um.bio.olemiss.edu>
2018-02-19 17:03:44 -0600

    overlaying movement traces onto landscapes

--- movement_plot_functions.R
+++ movement_plot_functions.R
@@ -1,11 +1,13 @@
 ## Movement plots
 
-traceMovement <- function(trace.data, landscape){
+traceMovement <- function(trace.data, landscape, overlay = TRUE){
     
   locations <- trace.data
   A <- landscape
   
-  plot(1,1, cex=0, xlim=c(0,ncol(A)), ylim=c(0,ncol(A)))
+  if (overlay == TRUE){
+  
+  points(1,1, cex=0, xlim=c(0,ncol(A)), ylim=c(0,ncol(A)))
     points(locations[1,1], locations[1,2], pch = 19)
     points(locations[time,1], locations[time,2], pch = 19, col = 2)
     for (i in 2:time){
@@ -14,7 +16,19 @@ traceMovement <- function(trace.data, landscape){
              x1 = locations[i,1], y1 = locations[i,2],
              length = 0)
     }
+  } else {
+          plot(1,1, cex=0, xlim=c(0,ncol(A)), ylim=c(0,ncol(A)))
+          points(locations[1,1], locations[1,2], pch = 19)
+          points(locations[time,1], locations[time,2], pch = 19, col = 2)
+          for (i in 2:time){
+            points(locations[i,1], locations[i,2], pch = 19, col =rgb(0,0,0,0.5), cex = 0.5)
+            arrows(x0 = locations[i-1,1], y0 = locations[i-1,2],
+                   x1 = locations[i,1], y1 = locations[i,2],
+                   length = 0)
+          }
+        }
 }
+
 
 # ## Plot the disperasl kernal from a focal indidivual
 # kernalPlot <- function(landscape){
