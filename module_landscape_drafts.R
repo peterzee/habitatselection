3da21f2fa68e7ad858bf1d5e9156dea7ce6045c7
Peter Zee <peterzee@hst000171421um.bio.olemiss.edu>
2018-02-19 16:43:40 -0600

    tracking movement among modules

--- module_landscape_drafts.R
+++ module_landscape_drafts.R
@@ -1,12 +1,10 @@
 source('plotLandscape_function.R')
 
-
-
-
-  module.dim <- 10
-  hab.matrix <- 2
+  hab.matrix <- 4
   patch.dim <- 4
+  module.dim <- 6
   
+    
   mod.index <- array(1:module.dim^2, dim = c(module.dim, module.dim))
   tmp.ind <- which(mod.index > 0, arr.ind = TRUE)
   
@@ -20,7 +18,6 @@ source('plotLandscape_function.R')
   module.extract <- array(dim = c((hab.matrix + patch.dim), (hab.matrix + patch.dim), length(mod.index)))
     
   count <- 0
-
   
   risk.vector <- c(seq(0, 2, length.out = module.dim/2), rev(seq(0, 2, length.out = module.dim/2)))
     
@@ -31,11 +28,11 @@ source('plotLandscape_function.R')
     hab.module <- array(0, dim = c(hab.matrix + patch.dim, hab.matrix + patch.dim))
     
     
-    x <- array(sample(0:2, patch.dim^2, prob = c(1,1,1), replace = TRUE), dim = c(patch.dim, patch.dim))
+    # x <- array(sample(0:2, patch.dim^2, prob = c(0,1,1), replace = TRUE), dim = c(patch.dim, patch.dim))
     
     ### heterogeneity among modules
     module.col <- (which(unique(quilted.index[,2]) == quilted.index[i,2]))
-    # x <- array(sample(0:2, patch.dim^2, prob = c(0, 1, risk.vector[module.col]), replace = TRUE), dim = c(patch.dim, patch.dim))
+    x <- array(sample(0:2, patch.dim^2, prob = c(0, 1, risk.vector[module.col]), replace = TRUE), dim = c(patch.dim, patch.dim))
     
 
     hab.module[((hab.matrix / 2) + 1) : (((hab.matrix / 2)) + patch.dim), ((hab.matrix / 2) + 1) : (((hab.matrix / 2)) + patch.dim) ] <- x
--- module_neighborhoods.R
+++ module_neighborhoods.R
@@ -1,5 +1,4 @@
 ## module neighborhoods
-
 # quilted
 # A
 # module.extract
@@ -13,9 +12,10 @@
 # tapply(egg.landscape[module.extract[,,6]], A[module.extract[,,6]], mean)
 
 
+
 ## module summary
-module.table <- array(dim = c(module.dim^2, 8))
-colnames(module.table) <- c('id', 'n.empty', 'n.safe', 'n.risky', 'n.egg.safe', 'n.egg.risky', 'mean.egg.safe', 'mean.egg.risky')
+module.table <- array(dim = c(module.dim^2, 9))
+colnames(module.table) <- c('id', 'n.empty', 'n.safe', 'n.risky', 'n.egg.safe', 'n.egg.risky', 'mean.egg.safe', 'mean.egg.risky', 'module.risk')
 for (i in 1:nrow(module.table)){
   
   module.table[i,1] <- i 
@@ -27,7 +27,54 @@ for (i in 1:nrow(module.table)){
                            sum(egg.landscape[module.extract[,,i]][which(A[module.extract[,,i]] == 2)]))
   module.table[i,7:8] <- c(sum(egg.landscape[module.extract[,,i]][which(A[module.extract[,,i]] == 1)]) / sum(A[module.extract[,,i]] == 1),
                            sum(egg.landscape[module.extract[,,i]][which(A[module.extract[,,i]] == 2)]) / sum(A[module.extract[,,i]] == 2))
-  
+  module.table[i,9] <- module.table[i,6] / sum(module.table[i,2:4])
 }
 
-plot(module.table[,"n.risky"], module.table[,"mean.egg.safe"])
+plot(module.table[,"n.risky"], module.table[,"mean.egg.safe"],
+     xlab = 'number of risky patches in module', ylab = 'mean eggs / safe patch in module')
+
+
+## adjacnt modules (basically a moore neighborhood for modules)
+module.moore <-array(dim = c(nrow(mod.index), ncol(mod.index), length(mod.index)))
+for (i in 1:length(mod.index)){
+  
+  ROW <- which(mod.index == i, arr.ind = TRUE)[1]
+  COL <- which(mod.index == i, arr.ind = TRUE)[2]
+  
+  range <- 1
+  
+  row.range <- (ROW - range):(ROW + range)
+    row.range[row.range <= 0] <- row.range[row.range <= 0] - 1
+    row.range[row.range > (nrow(mod.index))] <- row.range[row.range > (nrow(mod.index))] + 1
+  
+  
+  col.range <- (COL - range):(COL + range)
+    col.range[col.range <= 0] <- col.range[col.range <= 0] - 1
+    col.range[col.range > (ncol(mod.index))] <- col.range[col.range > (ncol(mod.index))] + 1
+  
+  v.range <- row.range %% (nrow(mod.index)+1)
+  h.range <- col.range %% (ncol(mod.index)+1)
+  
+  module.moore[,,i] <- mod.index[v.range, h.range]
+  module.moore[range + 1, range + 1,i] <- NA  
+}
+
+
+mean.module.risk <- rep(NA, nrow(module.table))
+for (i in 1:nrow(module.table)){
+  mean.module.risk[i]<- mean(module.table[module.moore[,,i], 'module.risk'], na.rm = TRUE)
+}
+
+plot(mean.module.risk, module.table[,"mean.egg.safe"],
+     xlab = 'mean risk of surrounding modules', ylab = 'eggs / safe patch in focal module')
+
+
+## module tracking (location)
+module.tracker <- array(dim = c(time, pop.size))
+change.module <- rep(NA, pop.size) 
+for (j in 1:pop.size){
+  for (i in 1:time){
+    module.tracker[i,j] <- which(module.extract == index[locations[i,1,j], locations[i,2,j]], arr.ind = TRUE)[3]
+    }
+    change.module[j] <- sum(!is.na(rle(module.tracker[,j])$values))-1
+}
