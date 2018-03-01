04de049dd952efd55b2218dada669052f45cdcc1
Peter Zee <peterzee@hst000171421um.bio.olemiss.edu>
2018-03-01 11:50:42 -0600

    started on within patch, juvenile dyanmics

---
+++ within.patch.dynamics.R
@@ -0,0 +1,25 @@
+## Population and community dynamics within a patch
+
+## Bunch of eggs laid
+## Hatch into juveniles
+## Juveniles compete for resources within patch (intra and interspecific)
+## Juveniles subject to predation 
+## Emergence
+
+
+## baseline juvenile rates
+hatch.rate <- 1
+death.rate <- 0.1
+
+## baseline predation rate
+pred.death <- 0.4
+
+
+
+
+
+
+time.in.patch <- 10
+
+
+runif(time.in.patch) < 0.1
