9a5edff3471349292b84c6d43ce1b861abbce193
Peter Zee <peterzee@hst000171421um.bio.olemiss.edu>
2018-03-19 12:40:13 -0500

    cleaning some s

---
+++ parameters.R
@@ -0,0 +1,8 @@
+### Parameters for running everything
+
+## Lanscape parameters
+params_landscape <- list()
+
+## Population parameters
+params_population <- list()
+
--- run_everything.R
+++ run_everything.R
@@ -1,10 +1,11 @@
 ## Run everything together (habitat selection)
-
+source('install_packages.R')
 source('header.R')
 
 
 
 ## Parameter values
+source('parameters.R')
 
 ## Generate landscape(s)
 
